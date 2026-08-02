f0c95885cf9612a70edbaa753d6889debd529107
egoist0690 <pickycreepy@gmail.com>
2026-08-02 18:00:23 +0530

    Update owner.py

--- filters/owner.py
+++ filters/owner.py
@@ -1,1 +1,9 @@
+from aiogram.filters import Filter
+from aiogram.types import Message, CallbackQuery
 
+from config import OWNER_ID
+
+
+class OwnerFilter(Filter):
+    async def __call__(self, event: Message | CallbackQuery) -> bool:
+        return event.from_user.id == OWNER_ID
