bc5c275800dc40e5eb265a32039240383d0a20ed
egoist0690 <pickycreepy@gmail.com>
2026-08-02 17:57:47 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,10 @@
 
+import html
+
+
+def format_number(num: int) -> str:
+    return f"{num:,}"
+
+
+def get_mention(user_id: int, name: str) -> str:
+    return f"<a href='tg://user?id={user_id}'>{html.escape(name)}</a>"
