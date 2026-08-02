82210ceb62f622b75ebb7003fbd976813247eb0f
egoist0690 <pickycreepy@gmail.com>
2026-08-02 18:09:19 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
+# ==========================================
+# Waifu Bot
+# Telegram: @MrZyro_dev
+# GitHub: https://github.com/MrZyro
+# ==========================================
 
+__version__ = "2.0.0"
+__author__ = "MrZyro"
