54381661f9b08d2510b9b2607932b521f8f5f51d
egoist0690 <pickycreepy@gmail.com>
2026-08-02 18:27:15 +0530

    Update __init__.py

--- callbacks/__init__.py
+++ callbacks/__init__.py
@@ -1,7 +1,15 @@
-
 from aiogram import Dispatcher
 from callbacks.help import router as help_router
+from callbacks.harem import router as harem_router
+from callbacks.check import router as check_router
+from callbacks.search import router as search_router
+from callbacks.gift import router as gift_router
 
 
 def register_all_callbacks(dp: Dispatcher):
+    """Register all callback handlers."""
     dp.include_router(help_router)
+    dp.include_router(harem_router)
+    dp.include_router(check_router)
+    dp.include_router(search_router)
+    dp.include_router(gift_router)
